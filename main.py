c9a47716012acb00cc4f7201565cab0484972f70
wflorian94 <93941015+wflorian94@users.noreply.github.com>
2022-01-03 20:04:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,46 @@ Snake game made with pygame
 """
 
 import pygame
+import random
+
+
+
+
+
+class food:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.x = random.randint(20, self.width - 20)
+        self.y = random.randint(20, self.height - 20)
 
 
 class App:
+
+    orange = (0xFF, 0x8C, 0x00)
+
+
     def __init__(self):
         self._game_active = True
         self.game_board = None
-        self.size = self.weight, self.height = 640, 480
+        self.width = 640
+        self.height = 480
+        self.size = self.width, self.height
         self.caption = None
+        self.clock = pygame.time.Clock()
+        self.food = food(self.width, self.height)
 
     def on_init (self):
         pygame.init()
         self.game_board = pygame.display.set_mode(self.size, pygame.HWSURFACE)
-        self._caption = pygame.display.set_caption("Snake - eat em all")
+        self.caption = pygame.display.set_caption("Snake - eat em all")
         self._game_active = True
+        self.clock.tick(25)
 
     #chek if user hits the Quit-button
     def on_event (self, event):
+    
+
         if event.type == pygame.QUIT:
             self._game_active = False
 
@@ -30,12 +53,14 @@ class App:
 
         #computes changes in the game. movements of snake etc 
 
-    def on_render (self):
-        pass
+    def on_render (self, food):
+        pygame.draw.rect(self.game_board, self.orange, pygame.Rect(self.food.x, self.food.y, 20, 20)) #20 20 is the size of the food
+        pygame.display.flip()
+
 
         #print out the screen graphic
 
-
+        
     def on_cleanup(self):
         pygame.quit()
 
@@ -49,7 +74,7 @@ class App:
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
-            self.on_render()
+            self.on_render(self.food)
         self.on_cleanup()
 
 if __name__ == "__main__":
